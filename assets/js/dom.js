20a9a3687fff881f167c76f6c764e4b400b38241
Yuliana Salazar <eysalazar75@misena.edu.co>
2022-04-01 11:06:19 -0500

    first commit

---
+++ assets/js/dom.js
@@ -0,0 +1,30 @@
+
+const nombre = document.getElementById("nombre");
+const hola = document.getElementById("hola");
+const numero  = document.getElementById("numero");
+
+let numero1 = prompt("Ingrese el primer numero");
+let numero2 = prompt("Ingrese el segundo numero");
+
+if(numero1 ==  numero2)
+{
+    console.log(numero);
+
+}else{
+   
+}
+let nuevoNombre = prompt("Ingrese un nombre");
+
+nombre.textContent = nuevoNombre;
+
+let nombre= prompt("Ingrese un numero");
+ let a = 2;
+let suma = parseInt(numero) + a;
+ alert(suma);
+hola.textContent = suma;
+console.log(nombre);
+
+
+
+//TODO: Consultar los operadores relacionales ==, ===, !=, !==
+//TODO: Consultar los operadores logicos para AND, OR, NOT
---
+++ assets/js/operaciones.js
@@ -0,0 +1,23 @@
+
+let  numero = Math.floor(Math.random() * 100) +1;
+
+console.log(numero);
+
+let intento = 50;
+
+if(intento > numero)
+{
+    console.log("El numero ingresado es mayor.");
+} else if(intento < numero) {
+    console.log("El numero ingresado es menor.");
+}else{
+    console.log("Has adivinado el numero.");
+}
+let numero1 = 2
+let numero2 = 4
+if(numero1 == numero2)
+{
+    console.log("los numeros son iguales");
+}else {
+    console.log("no son iguales");
+}
